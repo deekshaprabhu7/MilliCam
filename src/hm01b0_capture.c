88115d0337c358b29c47b99854b97f1885354957
Deeksha Prabhu <deekshaprabhu795@gmail.com>
2024-04-20 12:29:56 -0700

    Check Camera Version

--- src/hm01b0_capture.c
+++ src/hm01b0_capture.c
@@ -5,6 +5,18 @@ LOG_MODULE_REGISTER(hm01b0capture, CONFIG_LOG_DEFAULT_LEVEL);
 
 void hm01b0_init(void)
 {
-    int ret;
+    /*Test if camera is functional*/
+    hm_i2c_write(REG_MODE_SELECT,0x00);
+    uint8_t version = hm_i2c_read(REG_MODEL_ID_L);
+    if(version != 0xB0){
+        LOG_INF("REG_MODEL_ID_L: 0x%x", version);
+        LOG_INF("Camera version problem");
+    }
+    else
+    {
+        LOG_INF("Camera version: 0x%02x", version);
+    }
 
-} 
+    hm_i2c_write( REG_MODE_SELECT, 0x00); //go to stand by mode
+
+}
